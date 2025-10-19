53e894b37fe05a545f7075211f7dee68291fbc9d
Albee Akbar Fillah <albeefillah17@gmail.com>
2025-10-19 17:17:40 +0700

    konfigurasi controller dokumen

--- app/Http/Controllers/Frontend/DokumenController.php
+++ app/Http/Controllers/Frontend/DokumenController.php
@@ -7,5 +7,48 @@ use Illuminate\Http\Request;
 
 class DokumenController extends Controller
 {
-    //
+    public function standarPelayanan()
+    {
+        return view('frontend.dokumen.standar-pelayanan');
+    }
+    public function sop()
+    {
+        return view('frontend.dokumen.sop');
+    }
+    public function lakipLaptah()
+    {
+        return view('frontend.dokumen.lakip-laptah');
+    }
+    public function renstraPPSDMA()
+    {
+        return view('frontend.dokumen.renstra-ppsdma');
+    }
+    public function renstraBPSDM()
+    {
+        return view('frontend.dokumen.renstra-bpsdm');
+    }
+    public function ikm()
+    {
+        return view('frontend.dokumen.ikm');
+    }
+    public function ipk()
+    {
+        return view('frontend.dokumen.ipk');
+    }
+    public function zonaIntegritas()
+    {
+        return view('frontend.dokumen.zona-integritas');
+    }
+    public function petaResiko()
+    {
+        return view('frontend.dokumen.peta-resiko');
+    }
+    public function kebijakanAntiPenyuapan()
+    {
+        return view('frontend.dokumen.kebijakan-anti-penyuapan');
+    }
+    public function laporanAktualisasi()
+    {
+        return view('frontend.dokumen.laporan-aktualisasi');
+    }
 }
